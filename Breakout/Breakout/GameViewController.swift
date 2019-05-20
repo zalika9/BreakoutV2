e67fdb57321743c3906507abeb6ca2583145d753
shalai21 <49201188+shalai21@users.noreply.github.com>
2019-05-20 10:04:02 -0500

    Added directions to Breakout

--- Breakout/Breakout/GameViewController.swift
+++ Breakout/Breakout/GameViewController.swift
@@ -14,23 +14,6 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-            
-        }
     }
 
     override var shouldAutorotate: Bool {
@@ -44,17 +27,17 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    @IBAction func onDirectionsButtonPressed(_ sender: UIBarButtonItem) {
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
     }
     
-    @IBAction func onRestartButtonPressed(_ sender: UIBarButtonItem) {
-        let gameScene = GameScene()
-        gameScene.restartGame()
+    
+    @IBAction func directionsButton(_ sender: UIButton) {
     }
     
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
